83c493fc8a068c076acae58a01ddd691943f7750
Daniel Opitz <opitz.daniel.82@gmail.com>
2018-03-10 01:20:02 +0100

    Fixed coding style

--- src/Cache/Exception/InvalidArgumentException.php
+++ src/Cache/Exception/InvalidArgumentException.php
@@ -6,5 +6,4 @@ use Psr\SimpleCache\InvalidArgumentException as SimpleCacheInvalidArgumentExcept
 
 class InvalidArgumentException extends \Exception implements SimpleCacheInvalidArgumentException
 {
-
 }
